e85610b062765bef170ffde706090fe9347eb1ff
vgarpre <vgarpre@vgarprelinux.educacion.org>
2020-02-13 12:54:29 +0000

    routing patched to work on rails 5. still needs more work

--- config/routes.rb
+++ config/routes.rb
@@ -1,131 +1,36 @@
 def rb_match(object, path, hash)
-  if Rails::VERSION::MAJOR < 3
-    hash[:controller] = hash[:to].split('#')[0].to_sym
-    hash[:action] = hash[:to].split('#')[1]
-    hash.delete(:to)
-    if hash[:via]
-      hash[:conditions] = { :method => hash[:via] }
-      hash.delete(:via)
-    end
-    object.connect path, hash
-  else
-    match path, hash
-  end
+  match path, hash
 end
 
 def rb_common_routes(rb)
   rb_match rb, 'releases/:project_id',
-               :to => 'rb_releases#index', :via => [:get]
+  :to => 'rb_releases#index', :via => [:get]
   rb_match rb, 'release/:project_id/new',
-               :to => 'rb_releases#new', :via => [:get, :post]
+  :to => 'rb_releases#new', :via => [:get, :post]
   rb_match rb, 'release/:release_id',
-               :to => 'rb_releases#show', :via => [:get]
+  :to => 'rb_releases#show', :via => [:get]
   rb_match rb, 'release/:release_id',
-               :to => 'rb_releases#destroy', :via => [:delete]
+  :to => 'rb_releases#destroy', :via => [:delete]
   rb_match rb, 'release/:release_id/edit',
-               :to => 'rb_releases#edit', :via => [:get, :post]
+  :to => 'rb_releases#edit', :via => [:get, :post]
   rb_match rb, 'release/:release_id/shapshot',
-               :to => 'rb_releases#snapshot', :via => [:get]
+  :to => 'rb_releases#snapshot', :via => [:get]
 
 end
 
-if Rails::VERSION::MAJOR < 3
-ActionController::Routing::Routes.draw do |map|
-  # Use rb/ as a URL 'namespace.' We're using a slightly different URL pattern
-  # From Redmine so namespacing avoids any further problems down the line
-  map.resource :rb, :only => :none do |rb|
-    rb.resource   :updated_items,    :only => :show,                :controller => :rb_updated_items,   :as => "updated_items/:project_id"
-    rb.resource   :wiki,             :only => [:show, :edit],       :controller => :rb_wikis,           :as => "wikis/:sprint_id"
-    rb.resource   :task,             :except => :index,             :controller => :rb_tasks,           :as => "task/:id"
-    rb.resources  :tasks,            :only => :index,               :controller => :rb_tasks,           :as => "tasks/:story_id"
-    rb.resource   :taskboard,        :only => :show,                :controller => :rb_taskboards,      :as => "taskboards/:sprint_id"
 
-    rb_common_routes rb
+resource :rb, :only => :none do |rb|
 
-    rb_match rb, 'issues/backlog/product/:project_id',
-                 :to => 'rb_queries#show'
-    rb_match rb, 'issues/backlog/sprint/:sprint_id',
-                 :to => 'rb_queries#show'
-    rb_match rb, 'issues/impediments/sprint/:sprint_id',
-                 :to => 'rb_queries#impediments'
+    # releases
+  #  resources :projects do
+  #    resources :releases, :only => [:index, :new,:show, :edit, :destroy, :snapshot], :controller => :rb_releases  do
+  #      get 'snapshot', :on => :member 
+  #      post 'edit', :on => :member
+  #      post 'new', :on => :member
+  #    end
+  #  end
 
-    rb_match rb, 'statistics',
-                 :to => 'rb_all_projects#statistics'
-
-    rb_match rb, 'server_variables/project/:project_id.js',
-                 :to => 'rb_server_variables#project', :format => 'js'
-    rb_match rb, 'server_variables/project/:project_id',
-                 :to => 'rb_server_variables#project', :format => nil
-    rb_match rb, 'server_variables/sprint/:sprint_id.js',
-                 :to => 'rb_server_variables#sprint', :format => 'js'
-    rb_match rb, 'server_variables/sprint/:sprint_id',
-                 :to => 'rb_server_variables#sprint', :format => nil
-    rb_match rb, 'server_variables.js',
-                 :to => 'rb_server_variables#index', :format => 'js'
-    rb_match rb, 'server_variables',
-                 :to => 'rb_server_variables#index', :format => nil
-
-    rb_match rb, 'master_backlog/:project_id',
-                 :to => 'rb_master_backlogs#show'
-    rb_match rb, 'master_backlog/:project_id/menu.json',
-                 :to => 'rb_master_backlogs#menu', :format => 'json'
-
-    rb_match rb, 'impediment/create',
-                 :to => 'rb_impediments#create'
-    rb_match rb, 'impediment/update/:id',
-                 :to => 'rb_impediments#update'
-
-    rb_match rb, 'sprint/create',
-                 :to => 'rb_sprints#create'
-    rb_match rb, 'sprint/:sprint_id/update',
-                 :to => 'rb_sprints#update'
-    rb_match rb, 'sprint/:sprint_id/reset',
-                 :to => 'rb_sprints#reset'
-    rb_match rb, 'sprint/download/:sprint_id.xml',
-                 :to => 'rb_sprints#download', :format => 'xml'
-    rb_match rb, 'sprints/:project_id/close_completed',
-                 :to => 'rb_sprints#close_completed'
-
-    rb_match rb, 'stories/sprint/:sprint_id.pdf',
-                 :to => 'rb_stories#index', :format => 'pdf'
-    rb_match rb, 'stories/project/:project_id.pdf',
-                 :to => 'rb_stories#index', :format => 'pdf'
-    rb_match rb, 'story/create',
-                 :to => 'rb_stories#create'
-    rb_match rb, 'story/update/:id',
-                 :to => 'rb_stories#update'
-
-    rb_match rb, 'calendar/:key/:project_id.ics',
-                 :to => 'rb_calendars#ical', :format => 'xml'
-
-    rb_match rb, 'burndown/:sprint_id',
-                 :to => 'rb_burndown_charts#show'
-    rb_match rb, 'burndown/:sprint_id/embed',
-                 :to => 'rb_burndown_charts#embedded'
-    rb_match rb, 'burndown/:sprint_id/print',
-                 :to => 'rb_burndown_charts#print'
-
-    rb_match rb, 'hooks/sidebar/sprint/:sprint_id',
-                 :to => 'rb_hooks_render#view_issues_sidebar'
-    rb_match rb, 'hooks/sidebar/project/:project_id',
-                 :to => 'rb_hooks_render#view_issues_sidebar'
-  end
-
-end
-
-else
-  resource :rb, :only => :none do |rb|
-
-  # releases
-#  resources :projects do
-#    resources :releases, :only => [:index, :new,:show, :edit, :destroy, :snapshot], :controller => :rb_releases  do
-#      get 'snapshot', :on => :member 
-#      post 'edit', :on => :member
-#      post 'new', :on => :member
-#    end
-#  end
-
-    rb_common_routes rb
+  rb_common_routes rb
 
   rb_match rb, 'updated_items/:project_id', :to => 'rb_updated_items#show', via: :get
 
@@ -139,29 +44,29 @@ else
   rb_match rb, 'tasks/:story_id', :to => 'rb_tasks#index', via: :get
 
   rb_match rb, 'taskboards/:sprint_id',
-            :to => 'rb_taskboards#show', via: :get
+  :to => 'rb_taskboards#show', via: :get
 
 
   rb_match rb, 'staticstics', :to => 'rb_all_projects#statistics', via: :get
 
   rb_match rb, 'server_variables/sprint/:sprint_id.js',
-              :to => 'rb_server_variables#sprint',
-              :format => 'js', via: :get
+  :to => 'rb_server_variables#sprint',
+  :format => 'js', via: :get
   rb_match rb, 'server_variables/sprint/:sprint_id.js',
-              :to => 'rb_server_variables#sprint',
-              :format => nil, via: :get
+  :to => 'rb_server_variables#sprint',
+  :format => nil, via: :get
   rb_match rb, 'server_variables.js',
-              :to => 'rb_server_variables#index',
-              :format => 'js', via: :get
+  :to => 'rb_server_variables#index',
+  :format => 'js', via: :get
   rb_match rb, 'server_variables.js',
-              :to => 'rb_server_variables#index',
-              :format => nil, via: :get
+  :to => 'rb_server_variables#index',
+  :format => nil, via: :get
   rb_match rb, 'server_variables/project/:project_id.js',
-              :to => 'rb_server_variables#project',
-              :format => 'js', via: :get
+  :to => 'rb_server_variables#project',
+  :format => 'js', via: :get
   rb_match rb, 'server_variables/project/:project_id.js',
-              :to => 'rb_server_variables#project',
-              :format => nil, via: :get
+  :to => 'rb_server_variables#project',
+  :format => nil, via: :get
 
   rb_match rb, 'master_backlog/:project_id', :to => 'rb_master_backlogs#show', via: :get
 
@@ -182,16 +87,16 @@ else
   rb_match rb, 'story/update/:id', :to => 'rb_stories#update', via: :post
 
   rb_match rb, 'calendar/:key/:project_id.ics', :to => 'rb_calendars#ical',
-          :format => 'xml', via: :get
+  :format => 'xml', via: :get
 
   rb_match rb, 'burndown/:sprint_id',         :to => 'rb_burndown_charts#show', via: :get
   rb_match rb, 'burndown/:sprint_id/embed',   :to => 'rb_burndown_charts#embedded', via: :get
   rb_match rb, 'burndown/:sprint_id/print',   :to => 'rb_burndown_charts#print', via: :get
 
   rb_match rb, 'hooks/sidebar/project/:project_id',
-          :to => 'rb_hooks_render#view_issues_sidebar', via: :get
+  :to => 'rb_hooks_render#view_issues_sidebar', via: :get
   rb_match rb, 'hooks/sidebar/project/:project_id/:sprint_id',
-          :to => 'rb_hooks_render#view_issues_sidebar', via: :get
-  end
+  :to => 'rb_hooks_render#view_issues_sidebar', via: :get
+
 end
 
